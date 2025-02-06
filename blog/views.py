bd1524621327a8e67a6b7e4a2e7ceeeac09865ce
kangyeon <rkddus0517@naver.com>
2025-02-06 22:27:53 +0900

    :sparkles: Create, Update 기능 추가

--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,12 @@ class PostList(ListView):
 class PostDetail(DetailView):
     model = Post
 
+
+class PostCreate(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+class PostUpdate(UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_update_form.html'
