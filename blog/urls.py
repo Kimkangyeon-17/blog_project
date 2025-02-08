fb5e2219bd6acac25eadddb9a438f49e9066aa0b
kangyeon <rkddus0517@naver.com>
2025-02-08 19:10:39 +0900

    :white_check_mark: Add Category model

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('delete_post/<int:pk>/', views.PostDelete.as_view()),
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/', views.PostCreate.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, UpdateView, CreateView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
+from django.urls import reverse_lazy
 
 class PostList(ListView):
     model = Post
@@ -10,11 +12,25 @@ class PostDetail(DetailView):
     model = Post
 
 
-class PostCreate(CreateView):
+class PostCreate(LoginRequiredMixin ,CreateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['title', 'content', 'head_image', 'file_upload']
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated :
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
+
 
 class PostUpdate(UpdateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['title', 'content', 'head_image', 'file_upload']
     template_name = 'blog/post_update_form.html'
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy("post_list")
