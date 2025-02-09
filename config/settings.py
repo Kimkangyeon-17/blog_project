ea79eb94d770e80ef7c8cf794200fb096a9758ef
kangyeon <rkddus0517@naver.com>
2025-02-09 22:36:13 +0900

    :lipstick: Crispy form 적용

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,10 @@ INSTALLED_APPS = [
     'blog',
     'home_page',
     'accounts',
+
+    "crispy_forms",
+    "crispy_bootstrap5",
+
 ]
 
 MIDDLEWARE = [
@@ -116,3 +120,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
