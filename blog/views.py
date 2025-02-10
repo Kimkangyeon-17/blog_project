26e46423772bae6c9cbc9b8f3fecc6fc953a7a6f
kangyeon <rkddus0517@naver.com>
2025-02-10 14:18:00 +0900

    :white_check_mark: Update comment edit, delete

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Post, Category
+from .models import Post, Category, Comment
 from django.urls import reverse_lazy
 from django.core.exceptions import PermissionDenied
+from .forms import CommentForm
 
 class PostList(ListView):
     model = Post
@@ -18,6 +19,13 @@ class PostList(ListView):
 
 class PostDetail(DetailView):
     model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        context['comment_form'] = CommentForm
+        return context
 
 
 class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
@@ -70,3 +78,48 @@ def category_page(request, slug):
     'category' : category,
         }
     )
+
+class CommentCreate(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+
+    def form_valid(self, form):
+        # post 객체 가져오기
+        self.object = form.save(commit=False)
+        self.object.post = get_object_or_404(Post, pk=self.kwargs['pk'])
+        self.object.author = self.request.user
+        return super().form_valid(form)
+
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+
+
+from django.views.generic import View
+
+
+class CommentDelete(LoginRequiredMixin, View):
+    def post(self, request, *args, **kwargs):
+        comment = get_object_or_404(Comment, pk=kwargs['pk'])
+
+        if request.user == comment.author:
+            post_url = comment.post.get_absolute_url()
+            comment.delete()
+            return redirect(post_url)
+        else:
+            raise PermissionDenied
+
+    def get(self, request, *args, **kwargs):
+        comment = get_object_or_404(Comment, pk=kwargs['pk'])
+        if request.user == comment.author:
+            post_url = comment.post.get_absolute_url()
+            comment.delete()
+            return redirect(post_url)
+        else:
+            raise PermissionDenied
